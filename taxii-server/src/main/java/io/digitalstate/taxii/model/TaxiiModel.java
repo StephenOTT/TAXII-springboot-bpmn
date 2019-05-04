2069e4afa2c831707bf14e918de9ee831e295198
StephenOTT <stephen@digitalstate.ca>
2019-05-04 16:52:06 -0400

    Add toJson method to default interface

--- taxii-server/src/main/java/io/digitalstate/taxii/model/TaxiiModel.java
+++ taxii-server/src/main/java/io/digitalstate/taxii/model/TaxiiModel.java
@@ -1,6 +1,19 @@
 package io.digitalstate.taxii.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.digitalstate.taxii.common.TaxiiParsers;
+import org.immutables.value.Value;
+
 import java.io.Serializable;
 
 public interface TaxiiModel extends Serializable {
+
+    @Value.Lazy
+    default String toJson() {
+        try {
+            return TaxiiParsers.getJsonMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("Unable to parse into json string", e);
+        }
+    }
 }
