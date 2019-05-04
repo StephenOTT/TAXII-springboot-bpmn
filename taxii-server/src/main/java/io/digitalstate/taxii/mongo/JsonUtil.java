90e8de0c1a4d7094573657bd76fa5f93dba4c693
StephenOTT <stephen@digitalstate.ca>
2019-05-04 16:50:42 -0400

    update helper to be more generic

--- taxii-server/src/main/java/io/digitalstate/taxii/mongo/JsonUtil.java
+++ taxii-server/src/main/java/io/digitalstate/taxii/mongo/JsonUtil.java
@@ -1,23 +1,27 @@
 package io.digitalstate.taxii.mongo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.digitalstate.stix.json.StixParsers;
 import io.digitalstate.taxii.common.TaxiiParsers;
+import io.digitalstate.taxii.model.TaxiiModel;
 import io.digitalstate.taxii.mongo.model.TaxiiMongoModel;
 
 import java.util.List;
 
 public class JsonUtil {
     /**
-     * Converts a List of TaxiiMongoModel objects into a Json list using the Json Mapper
+     * Converts a List of objects into a Json list using the Json Mapper
+     * Uses the TaxiiParsers JsonMapper which is extended from the StixParser JsonMapper
      * @param list
      * @return
      */
-    public static String ListToJson(List<? extends TaxiiMongoModel> list){
+    public static String ListToJson(List list){
         try {
             return TaxiiParsers.getJsonMapper().writeValueAsString(list);
         } catch (JsonProcessingException e) {
             throw new IllegalStateException("Unable to parse into json string", e);
         }
     }
+
 
 }
